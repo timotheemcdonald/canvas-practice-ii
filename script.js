92a7cb4d10025935cbe73c77939d09bd20073d76
timotheemcdonald <timotheemcdonald@gmail.com>
2021-02-16 18:23:39 -0500

    particles randomly draw and shrink

--- script.js
+++ script.js
@@ -28,20 +28,21 @@ canvas.addEventListener('mousemove', function(event){
 
 class Particle{
     constructor(){
-        this.x = mouse.x;
-        this.y = mouse.y;
-        this.size = Math.random() * 5 + 1;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 15 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.05;
     }
     draw(){
         ctx.fillStyle = 'blue';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
     }
 }
@@ -57,6 +58,10 @@ function handleParticles(){
     for(let i = 0; i < particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray[i].draw();
+        if (particlesArray[i].size <= 0.3){
+            particlesArray.splice(i, 1)
+            i--;
+        }
     }
 }
 
